6f62554e65e9226272cb44c7fbd8646c89a1bf6c
Amos Dong-Keon Kim <amos@shallwefootball.com>
2014-12-22 15:06:39 +0900

    modified query to detailed

--- app/models/leagueModel.js
+++ app/models/leagueModel.js
@@ -5,8 +5,8 @@ var db = require('../config/database');
 exports.selectLeague = function(leagueId, callback) {
     db.pool.acquire(function(err, conn){
         if(err) console.error('db - err : ', err);
-        conn.query('select leagueId, community, season from league where leagueId = ?', leagueId, function (err, result) {
-            if (err) console.error('err : ', err);
+        conn.query('select leagueId, community, season, start, end from league where leagueId = ?', leagueId, function (err, result) {
+            if (err) return console.error('err : ', err);
 
             callback(err, result[0]);
         });
