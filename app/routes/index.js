a3c6b6fc039ff57c781e003fb6613e5b0bdf468c
Amos Dong-Keon Kim <amos@shallwefootball.com>
2014-12-22 17:20:15 +0900

    modified auth

--- app/routes/index.js
+++ app/routes/index.js
@@ -22,79 +22,79 @@ API.league = require('../controllers/API/leagueAPI');
 
 //front-end route
 Route
-    .get ('/league/:leagueId/*', API.league.selectLeague)
+	.get ('/league/:leagueId/*', API.league.selectLeague)
 
-    .get  ('/myInfo', Auth.requiresLogin, userController.renderMyInfoView)
-    .post ('/updateProfileImg', userController.updateProfileImg)
-    .put  ('/updatePassword', userController.updatePassword)
-
-
+	.get  ('/myInfo', Auth.requiresLogin, userController.renderMyInfoView)
+	.post ('/updateProfileImg', userController.updateProfileImg)
+	.put  ('/updatePassword', userController.updatePassword)
 
 
-    //user route
-    //로그인, 회원가입
-    .get   ('/login',   authenticateController.renderLoginView)
-    .get   ('/signup',  authenticateController.renderSignupView)
-    .post  ('/login',   authenticateController.login)
-    //not get. post???
-    .get   ('/logout',  authenticateController.logout)
-    .post  ('/signup',  authenticateController.signup)
-    .delete('/signout', authenticateController.signout)
-
-    .get   ('/', Auth.requiresLogin, leagueController.renderLeagueView)
-    .get   ('/league', leagueController.renderLeagueView)
-
-    .post  ('/league/:leagueId/team', teamController.createTeam)
-    .post  ('/league/:leagueId/joinLeague', teamController.joinLeague)
-
-    //club route
-    .get   ('/league/:leagueId/joinedClub', Auth.requiresLogin, clubController.renderJoinedClubsView)  //리그시작되기 전에
-    .get   ('/league/:leagueId/club', clubController.renderClubsView)
-    .get   ('/league/:leagueId/club/:clubId', Auth.requiresLogin, clubController.renderClubDetailView)
-    .post  ('/league/:leagueId/club', clubController.createClub)
-    .delete('/deleteClub', clubController.deleteClub)
-
-    .post  ('/signupClub', playerController.signupClub)
-    .delete('/signoutClub', playerController.signoutClub)
-
-    .put  ('/updateSquadNumber', playerController.updateSquadNumber)
-    .put  ('/updatePosition', playerController.updatePosition)
-
-    // match
-    .get  ('/league/:leagueId/match', matchController.renderMatchTimeLineView)
-    .get  ('/league/:leagueId', leagueController.selectLeague)//why?
-
-    //formation
-    .get  ('/league/:leagueId/formation/:clubId', formationController.renderFormationView)
-    .put  ('/league/:leagueId/formation/:clubId', formationController.saveFormation)
-
-    .post ('/sendLineups',       formationController.sendLineups)
 
 
-    .get    ('/league/:leagueId/records/:matchId/home/:homeClubId/away/:awayClubId', recordController.renderRecordView)
-    .post   ('/records/:matchId', recordController.insertRecord)
-    .post   ('/recordSubs/:matchId', recordController.insertRecordSubs)
-    .delete ('/records/:matchId', recordController.deleteRecord)
+	//user route
+	//로그인, 회원가입
+	.get   ('/login',   authenticateController.renderLoginView)
+	.get   ('/signup',  authenticateController.renderSignupView)
+	.post  ('/login',   authenticateController.login)
+	//not get. post???
+	.get   ('/logout',  authenticateController.logout)
+	.post  ('/signup',  authenticateController.signup)
+	.delete('/signout', authenticateController.signout)
+
+	.get   ('/', Auth.requiresLogin, leagueController.renderLeagueView)
+	.get   ('/league', leagueController.renderLeagueView)
+
+	.post  ('/league/:leagueId/team', teamController.createTeam)
+	.post  ('/league/:leagueId/joinLeague', teamController.joinLeague)
+
+	//club route
+	.get   ('/league/:leagueId/joinedClub', Auth.requiresLogin, clubController.renderJoinedClubsView)  //리그시작되기 전에
+	.get   ('/league/:leagueId/club', clubController.renderClubsView)
+	.get   ('/league/:leagueId/club/:clubId', Auth.requiresLogin, clubController.renderClubDetailView)
+	.post  ('/league/:leagueId/club', clubController.createClub)
+	.delete('/deleteClub', clubController.deleteClub)
+
+	.post  ('/signupClub', playerController.signupClub)
+	.delete('/signoutClub', playerController.signoutClub)
+
+	.put  ('/updateSquadNumber', playerController.updateSquadNumber)
+	.put  ('/updatePosition', playerController.updatePosition)
+
+	// match
+	.get  ('/league/:leagueId/match', matchController.renderMatchTimeLineView)
+	.get  ('/league/:leagueId', leagueController.selectLeague)//why?
+
+	//formation
+	.get  ('/league/:leagueId/formation/:clubId', Auth.requiresLogin, formationController.renderFormationView)
+	.put  ('/league/:leagueId/formation/:clubId', formationController.saveFormation)
+
+	.post ('/sendLineups', formationController.sendLineups)
 
 
-    //매치생성
-    .get('/genMatch', adminController.generateMatch)
-    .get('/insertDate', adminController.insertMatch)
+	.get    ('/league/:leagueId/records/:matchId/home/:homeClubId/away/:awayClubId', Auth.requiresLogin, recordController.renderRecordView)
+	.post   ('/records/:matchId', recordController.insertRecord)
+	.post   ('/recordSubs/:matchId', recordController.insertRecordSubs)
+	.delete ('/records/:matchId', Auth.requiresLogin, recordController.deleteRecord)
 
-    .post('/updatePasswordAdmin', function (req, res){
-        console.log('req.body   : ', req.body);
-        bcrypt.hash(req.body.password, 8, function(err, hash){
-            db.pool.acquire(function(err, conn) {
-                if(err) console.error('db - err : ', err);
-                conn.query('update user set password = ? where email = ?', [hash, req.body.email], function(err, result) {
-                    if(err) console.error('err : ', err);
 
-                    res.json({result : result});
-                });
-                db.pool.release(conn);
-            });
-        });
-    })
+	//매치생성
+	.get('/genMatch', adminController.generateMatch)
+	.get('/insertDate', adminController.insertMatch)
+
+	.post('/updatePasswordAdmin', function (req, res){
+		console.log('req.body   : ', req.body);
+		bcrypt.hash(req.body.password, 8, function(err, hash){
+			db.pool.acquire(function(err, conn) {
+				if(err) console.error('db - err : ', err);
+				conn.query('update user set password = ? where email = ?', [hash, req.body.email], function(err, result) {
+					if(err) console.error('err : ', err);
+
+					res.json({result : result});
+				});
+				db.pool.release(conn);
+			});
+		});
+	})
 
 
 
