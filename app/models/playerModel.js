c2f0d1b5887194d0ac029db35575d2f6752fef4d
Amos Dong-Keon Kim <amos@shallwefootball.com>
2014-12-25 06:33:40 +0900

    modified query. leagueStatus in selectLeague

--- app/models/playerModel.js
+++ app/models/playerModel.js
@@ -21,10 +21,10 @@ exports.selectPlayer = function (email, callback) {
     });
 };
 
-exports.selectJoinedLeagues = function (email, callback){
+exports.selectJoinedLeagues = function (userId, callback){
     db.pool.acquire(function (err, conn){
         if(err) return console.error('err : ', err);
-        conn.query("select c.leagueId, if (l.end < now(), 'end', if(l.start < now() and now() < l.end, 'playing', 'before')) status from user u left outer join player p on u.userId = p.userId left outer join club c on p.clubId = c.clubId left outer join league l on l.leagueId = c.leagueId where u.email = ? order by l.start", email, function(err, joinedLeagues) {
+        conn.query("select if(p.userId = c.leaderId, 'true', 'false') leader, c.leagueId, l.community, l.season, if (l.end < now(), 'end', if(l.start < now() and now() < l.end, 'playing', if (now() < l.start, 'before', null))) leagueStatus, c.teamId, p.clubId, c.leaderId, p.playerId, p.squadNumber, p.position, p.matchPosition, p.status playerStatus from player p left outer join club c on c.clubId = p.clubId left outer join league l on l.leagueId = c.leagueId where userId = ?", userId, function(err, joinedLeagues) {
             if (err) return console.error('err : ', err);
 
             callback(err, joinedLeagues);
