c0ee2ee39e66b078160070026e205b2497d3ed0a
Amos Dong-Keon Kim <amos@shallwefootball.com>
2014-12-30 17:17:00 +0900

    modified query that is transfered method. 

--- app/models/leagueModel.js
+++ app/models/leagueModel.js
@@ -3,8 +3,8 @@ var db = require('../config/database');
 exports.selectLeague = function(leagueId, userId, callback) {
     db.pool.acquire(function(err, conn){
         if(err) console.error('db - err : ', err);
-        conn.query("select l.leagueId, l.community, l.season, l.start, l.end, if (l.end < now(), 'end', if(l.start < now() and now() < l.end, 'playing', 'before')) status,if ((select p.userId from player p left outer join club c on c.clubId = p.clubId where p.userId = ? and c.leagueId = ?), 1, 0) userJoined from league l where l.leagueId = ?", [userId, leagueId, leagueId], function (err, result) {
-            if (err) return console.error('err : ', err);
+        conn.query("select l.leagueId, l.community, l.season, l.start, l.end, if (l.end < now(), 'end', if(l.start < now() and now() < l.end, 'playing', 'before')) status, (select sum(if ((p.status = 'transfered' or p.status = 'starting' or p.status = 'sub' or p.status = 'excepted'), true, false)) wait from player p left outer join club c on p.clubId = c.clubId where p.userId = ? and c.leagueId = ?) userJoined from league l where l.leagueId = ?", [userId, leagueId, leagueId], function (err, result) {
+            if (err) return console.error('selectLeague err : ', err);
 
             callback(err, result[0]);
         });
