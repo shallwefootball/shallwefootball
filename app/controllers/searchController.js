1ac393bdc40b7520a8e6dcbd7031467b713a1de1
Amos Dong-Keon Kim <amos@shallwefootball.com>
2015-01-02 16:02:23 +0900

    add search response no result.

--- app/controllers/searchController.js
+++ app/controllers/searchController.js
@@ -3,7 +3,15 @@ var searchModel = require('../models/searchModel');
 exports.player = function (req, res) {
 
 	searchModel.selectLikePlayer(req.params.query, function (err, results) {
-
-		res.json(results);
+		console.log('lengtH    : ', results);
+		if (results.length > 0) {
+			res.send(results);
+		}else {
+			console.log('true????    : ', results.length);
+			res.send([{
+				playerName : "Sorry :(",
+				teamName : "검색결과가 없습니다."
+			}]);
+		}
 	});
 }
