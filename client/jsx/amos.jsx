253c4d0202f45e62c773b49a90fb9da9d3e94e11
n3n amos <amos@N3Nui-Mac-mini.local>
2016-01-22 21:09:13 +0900

    add bootstrap package

--- client/jsx/amos.jsx
+++ client/jsx/amos.jsx
@@ -2,7 +2,8 @@ var Amos = React.createClass({
   render: function() {
     return (
       <div className="box">
-        hihi
+        hihi~~~~~
+        <button className="btn btn-default">btn</button>
       </div>
     )
   }
