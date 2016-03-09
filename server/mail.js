6a03f4070aa6c1ec538565257e04b4abd003b1c7
n3n amos <amos@shallwefootball.com>
2016-03-10 01:21:02 +0900

    test commit

--- server/mail.js
+++ server/mail.js
@@ -1,7 +1,7 @@
 Meteor.startup(function () {
   var smtp = require('../config.js').smtp;
 
-  // console.log("smtp  : ", smtp);
+  console.log("smtp  : ", smtp);
 
   process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
 });
